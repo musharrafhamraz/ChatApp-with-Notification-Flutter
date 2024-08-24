2e57c30553d9795c299e361f68a009ef3c356323
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-08-24 17:00:18 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/services/auth/auth_gate.dart';
+import 'package:chat_app/theme/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthGate(),
+      theme: lightMode,
+    );
+  }
+}
